34499b0c7515ed4d0fa8366514fac2d375ad0bf8
Manas Ranjan Bera <manasbera484@gmail.com>
2025-06-28 12:13:23 +0530

    First app

---
+++ ReactJS/vite-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Vite App</h1>
+      <h1>Vite App</h1>
+      <h1>Vite App</h1>
+      <h1>Vite App</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
